e2a63996b3a0b3936232b84fc3aab52f2188ce98
David Liang <wyliang0790@gmail.com>
2019-07-19 20:21:07 -0400

    lint

--- frontend/src/aws/index.tsx
+++ frontend/src/aws/index.tsx
@@ -1,5 +1,5 @@
-import React, {useState} from "react";
-var AWS = require('aws-sdk');
+import React, { useState } from "react";
+var AWS = require("aws-sdk");
 
 interface State {
   base64: string | undefined;
@@ -11,39 +11,130 @@ export class AWSMetrics extends React.Component<{}, State> {
 
     this.state = {
       base64: undefined
-    }
+    };
   }
 
-  componentDidMount(){
-      var metricWidget = {
-        "metrics": [
-            [ "AWS/EC2", "CPUUtilization", "InstanceId", "i-a6081333", { "stat": "Average", "id": "m0r0", "label": "Automation_Staging", "visible": false } ],
-            [ "...", "i-067ef8d114f85ac8b", { "stat": "Average", "id": "m0r1", "label": "quora-scrape", "visible": false } ],
-            [ "...", "i-023066bc0f84ecafe", { "stat": "Average", "id": "m0r2", "label": "nexus-production", "visible": false } ],
-            [ "...", "i-035c7d47201dd3a29", { "stat": "Average", "id": "m0r3", "label": "Production" } ],
-            [ "...", "i-0d88a4bf3eb6e023b", { "stat": "Average", "id": "m0r4", "label": "Staging" } ],
-            [ "...", "i-0ad0f02aceb64c123", { "stat": "Average", "id": "m0r5", "label": "ads-staging", "visible": false } ],
-            [ "...", "i-06672f18a1d097870", { "stat": "Average", "id": "m0r6", "label": "nexus-staging", "visible": false } ],
-            [ "...", "i-01428ea8b60cd8611", { "stat": "Average", "id": "m0r7", "label": "ads-production", "visible": false } ],
-            [ "...", "i-0ad4112dc40b06ed7", { "stat": "Average", "id": "m0r8", "label": "jenkins", "visible": false } ],
-            [ "...", "i-0d294a0d37e63b60f", { "stat": "Average", "id": "m0r9", "label": "Automation_Production", "visible": false } ],
-            [ "...", "i-019d5de2649d57a46", { "stat": "Average", "id": "m0r10", "label": "Automation_Dev", "visible": false } ],
-            [ "...", "i-06dfe6071d1565fff", { "stat": "Average", "id": "m0r11", "label": "jenkins-windows-runner", "visible": false } ],
-            [ "AWS/ElasticBeanstalk", "EnvironmentHealth", "EnvironmentName", "Production", { "stat": "Average", "visible": false } ],
-            [ "...", "Staging", { "stat": "Average", "visible": false } ]
+  componentDidMount() {
+    var metricWidget = {
+      metrics: [
+        [
+          "AWS/EC2",
+          "CPUUtilization",
+          "InstanceId",
+          "i-a6081333",
+          {
+            stat: "Average",
+            id: "m0r0",
+            label: "Automation_Staging",
+            visible: false
+          }
         ],
-        "title": "CPU Utilization Average",
-        "legend": {
-            "position": "right"
-        },
-        "copilot": true,
-        "view": "timeSeries",
-        "stacked": false,
-        "width": 1430,
-        "height": 250,
-        "start": "-PT3H",
-        "end": "P0D"
-    }
+        [
+          "...",
+          "i-067ef8d114f85ac8b",
+          { stat: "Average", id: "m0r1", label: "quora-scrape", visible: false }
+        ],
+        [
+          "...",
+          "i-023066bc0f84ecafe",
+          {
+            stat: "Average",
+            id: "m0r2",
+            label: "nexus-production",
+            visible: false
+          }
+        ],
+        [
+          "...",
+          "i-035c7d47201dd3a29",
+          { stat: "Average", id: "m0r3", label: "Production" }
+        ],
+        [
+          "...",
+          "i-0d88a4bf3eb6e023b",
+          { stat: "Average", id: "m0r4", label: "Staging" }
+        ],
+        [
+          "...",
+          "i-0ad0f02aceb64c123",
+          { stat: "Average", id: "m0r5", label: "ads-staging", visible: false }
+        ],
+        [
+          "...",
+          "i-06672f18a1d097870",
+          {
+            stat: "Average",
+            id: "m0r6",
+            label: "nexus-staging",
+            visible: false
+          }
+        ],
+        [
+          "...",
+          "i-01428ea8b60cd8611",
+          {
+            stat: "Average",
+            id: "m0r7",
+            label: "ads-production",
+            visible: false
+          }
+        ],
+        [
+          "...",
+          "i-0ad4112dc40b06ed7",
+          { stat: "Average", id: "m0r8", label: "jenkins", visible: false }
+        ],
+        [
+          "...",
+          "i-0d294a0d37e63b60f",
+          {
+            stat: "Average",
+            id: "m0r9",
+            label: "Automation_Production",
+            visible: false
+          }
+        ],
+        [
+          "...",
+          "i-019d5de2649d57a46",
+          {
+            stat: "Average",
+            id: "m0r10",
+            label: "Automation_Dev",
+            visible: false
+          }
+        ],
+        [
+          "...",
+          "i-06dfe6071d1565fff",
+          {
+            stat: "Average",
+            id: "m0r11",
+            label: "jenkins-windows-runner",
+            visible: false
+          }
+        ],
+        [
+          "AWS/ElasticBeanstalk",
+          "EnvironmentHealth",
+          "EnvironmentName",
+          "Production",
+          { stat: "Average", visible: false }
+        ],
+        ["...", "Staging", { stat: "Average", visible: false }]
+      ],
+      title: "CPU Utilization Average",
+      legend: {
+        position: "right"
+      },
+      copilot: true,
+      view: "timeSeries",
+      stacked: false,
+      width: 1430,
+      height: 250,
+      start: "-PT3H",
+      end: "P0D"
+    };
 
     AWS.config = new AWS.Config();
     AWS.config.accessKeyId = "AKIAXIPEH3LBXVKMWW6U";
@@ -53,38 +144,32 @@ export class AWSMetrics extends React.Component<{}, State> {
     var cw = new AWS.CloudWatch();
 
     var params = {
-        MetricWidget: JSON.stringify(metricWidget),
-        
-      };
+      MetricWidget: JSON.stringify(metricWidget)
+    };
 
     cw.getMetricWidgetImage(params, (err: any, data: any) => {
-      debugger
+      debugger;
       if (err) {
-          console.log(err, err.stack); // an error occurred
-      }
-      else{
+        console.log(err, err.stack); // an error occurred
+      } else {
         const base64 = this.arrayBufferToBase64(data.MetricWidgetImage);
         console.log(base64);
         this.setState({
           base64
         });
-      }        
+      }
     });
-
-      
   }
 
-  arrayBufferToBase64( bytes: Uint8Array ) {
-    var binary = '';
+  arrayBufferToBase64(bytes: Uint8Array) {
+    var binary = "";
     var len = bytes.byteLength;
     for (var i = 0; i < len; i++) {
-        binary += String.fromCharCode( bytes[ i ] );
+      binary += String.fromCharCode(bytes[i]);
     }
-    return window.btoa( binary );
-}
-  render() {
-      return (
-        <img src={`data:image/png;base64, ${this.state.base64}`} />
-      );
+    return window.btoa(binary);
   }
-};
+  render() {
+    return <img src={`data:image/png;base64, ${this.state.base64}`} />;
+  }
+}
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,13 @@
-import React, {useState} from "react";
+import React from "react";
 import ReactDOM from "react-dom";
-import { TabContent, TabPane, Card, Button, CardTitle, CardText, Row, Col, Nav, NavItem, NavLink } from 'reactstrap';
-var classnames = require('classnames');
-var AWS = require('aws-sdk');
+import {
+  TabContent,
+  TabPane,
+  Nav,
+  NavItem,
+  NavLink
+} from "reactstrap";
+var classnames = require("classnames");
 
 import "./style.scss";
 import { AWSMetrics } from "./aws/index";
@@ -13,30 +18,30 @@ interface State {
 }
 
 class App extends React.Component<{}, State> {
-  timeframes: {key: string; value: string}[] = [
+  timeframes: { key: string; value: string }[] = [
     {
-      key: '1h',
-      value: 'One Hour'
+      key: "1h",
+      value: "One Hour"
     },
     {
-      key: '6h',
-      value: 'Six Hours'
+      key: "6h",
+      value: "Six Hours"
     },
     {
-      key: '1d',
-      value: 'One Day'
+      key: "1d",
+      value: "One Day"
     },
     {
-      key: '1w',
-      value: 'One Week'
+      key: "1w",
+      value: "One Week"
     },
     {
-      key: '1M',
-      value: 'One Month'
+      key: "1M",
+      value: "One Month"
     },
     {
-      key: '6w',
-      value: 'Six Weeks'
+      key: "6w",
+      value: "Six Weeks"
     }
   ];
 
@@ -44,9 +49,9 @@ class App extends React.Component<{}, State> {
     super(props);
 
     this.state = {
-      activeTab: '1',
-      timeframe: '1h'
-    }
+      activeTab: "1",
+      timeframe: "1h"
+    };
   }
 
   toggle = (tab: string) => {
@@ -55,17 +60,19 @@ class App extends React.Component<{}, State> {
         activeTab: tab
       });
     }
-  }
+  };
 
   handleTimeframeChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     this.setState({
       timeframe: event.currentTarget.value
     });
-  }
+  };
 
   srcForChart = (id: string) => {
-    return `https://public.google.stackdriver.com/public/chart/${id}?drawMode=color&showLegend=true&theme=light&autoRefresh=true&timeframe=${this.state.timeframe}`
-  }
+    return `https://public.google.stackdriver.com/public/chart/${id}?drawMode=color&showLegend=true&theme=light&autoRefresh=true&timeframe=${
+      this.state.timeframe
+    }`;
+  };
 
   render() {
     return (
@@ -73,52 +80,79 @@ class App extends React.Component<{}, State> {
         <Nav tabs>
           <NavItem>
             <NavLink
-              className={classnames({ active: this.state.activeTab === '1' })}
-              onClick={() => { this.toggle('1'); }}
+              className={classnames({ active: this.state.activeTab === "1" })}
+              onClick={() => {
+                this.toggle("1");
+              }}
             >
               FlooringStores
             </NavLink>
           </NavItem>
           <NavItem>
             <NavLink
-              className={classnames({ active: this.state.activeTab === '2' })}
-              onClick={() => { this.toggle('2'); }}
+              className={classnames({ active: this.state.activeTab === "2" })}
+              onClick={() => {
+                this.toggle("2");
+              }}
             >
               SlingShot
             </NavLink>
           </NavItem>
         </Nav>
         <TabContent activeTab={this.state.activeTab}>
-        <TabPane tabId="1">
-        <div className="text-right mt-3 mr-5">
-          <select onChange={this.handleTimeframeChange}>
-            {this.timeframes.map((timeframe: {key: string; value: string}) =>
-              <option value={timeframe.key} key={timeframe.key}>{timeframe.value}</option>
-            )};
-          </select>
-        </div>
-          <div className="row">
+          <TabPane tabId="1">
+            <div className="text-right mt-3 mr-5">
+              <select onChange={this.handleTimeframeChange}>
+                {this.timeframes.map(
+                  (timeframe: { key: string; value: string }) => (
+                    <option value={timeframe.key} key={timeframe.key}>
+                      {timeframe.value}
+                    </option>
+                  )
+                )}
+                ;
+              </select>
+            </div>
+            <div className="row">
               <div className="col-6">
-                <iframe className="p-5 border-0" src={this.srcForChart("3825643149546100198")} width="100%" height="400" scrolling="no" seamless={true}></iframe>
+                <iframe
+                  className="p-5 border-0"
+                  src={this.srcForChart("3825643149546100198")}
+                  width="100%"
+                  height="400"
+                  scrolling="no"
+                  seamless={true}
+                />
               </div>
               <div className="col-6">
-                <iframe className="p-5 border-0" src={this.srcForChart("3825643149546099295")} width="100%" height="400" scrolling="no" seamless={true}></iframe>
+                <iframe
+                  className="p-5 border-0"
+                  src={this.srcForChart("3825643149546099295")}
+                  width="100%"
+                  height="400"
+                  scrolling="no"
+                  seamless={true}
+                />
               </div>
               <div className="col-6">
-                <iframe className="p-5 border-0" src={this.srcForChart("16339011581550173096")} width="100%" height="400" scrolling="no" seamless={true}></iframe>
+                <iframe
+                  className="p-5 border-0"
+                  src={this.srcForChart("16339011581550173096")}
+                  width="100%"
+                  height="400"
+                  scrolling="no"
+                  seamless={true}
+                />
               </div>
             </div>
-        </TabPane>
-        <TabPane tabId="2">
-          <AWSMetrics />
-        </TabPane>
-      </TabContent>
+          </TabPane>
+          <TabPane tabId="2">
+            <AWSMetrics />
+          </TabPane>
+        </TabContent>
       </div>
-  )
+    );
   }
-};
+}
 
-ReactDOM.render(
-    <App/>,
-    document.getElementById("root"),
-);
+ReactDOM.render(<App />, document.getElementById("root"));
